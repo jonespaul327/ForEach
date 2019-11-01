b625dd4798844478e3ce11522f11ab2b4aa87661
Paul Jones <chickenjones44@gmail.com>
2019-10-31 20:31:03 -0400

    Tested in Python, Java, C++
    Created and tested a for each loop in Python, Java, C++. This was much easier than I thought likely becasue I had some unexpected experience. (Thank you Python)

---
+++ forEach.java
@@ -0,0 +1,9 @@
+class forEach {
+    public static void main(String[] arg) {
+        int array[]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int x : array) {
+            System.out.println(x);
+        }
+    }
+}
